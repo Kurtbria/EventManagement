4f0288cbae42f3461a0fd414aba34b26d5b29538
EricNdivo <j.ericndivo@gmail.com>
2024-03-30 14:50:22 +0300

    ticketJPEG

--- eventmanagement/urls.py
+++ eventmanagement/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 #from paypal.standard.ipn import urls as paypal_urls
 #from paypal.standard.ipn.views import ipn
-from .import views
+from . import views 
 
 urlpatterns = [
     path('',views.signup, name='signup'),
@@ -13,7 +13,7 @@ urlpatterns = [
     path('buy_tickets',views.buy_tickets,name='buy_tickets'),
     path('credit_card',views.credit_card,name='credit_card'),
     path('charge/',views.charge,name="charge"),
-    path('success/<str:args>/',views.successMsg,name='successMsg'),
+    path('success/<str:args>/',views.success_msg,name='successMsg'),
     #path('payment_successful',views.payment_successful,name="payment_successful"),
     #path("payment_cancelled", views.payment_cancelled, namee="payment_cancelled"),
     #path('stripe_webhook',views.stripe_webhook, name="stripe_webhoook"),
@@ -21,6 +21,8 @@ urlpatterns = [
     #path('paypal/', include(paypal_urls)),
     #path('paypal/ipn/', ipn, name='paypal-ipn'),
     path('initiate_payment',views.initiate_payment,name='initiate_payment'),
-    
-    
+    path('ticket',views.ticket,name='ticket'),
+    path('generate_ticket/', views.generate_ticket, name='generate_ticket'),
 ]
+    
+    
--- eventmanagement/views.py
+++ eventmanagement/views.py
@@ -1,86 +1,64 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth.models import User, auth
-from requests import post
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
 import stripe
+import requests
+import random
+import string
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.urls import reverse
 from django.conf import settings
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
-#from paypal.standard.forms import PayPalPaymentsForm
-import requests
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-
 # M-Pesa API credentials
-CONSUMER_KEY = '7fVgJmnP7q62V1aFl0SzCAi5h1b6fegj29bP0dPpSlyR6AuI'
-CONSUMER_SECRET = 'GivlXkJxFJ2LvR8LGiLivXEkaJ8SPdhBHbO3zJkMvdrzUfxr8exxv1MkJBhgdQ4A'
+CONSUMER_KEY = 'your_consumer_key'
+CONSUMER_SECRET = 'your_consumer_secret'
 SHORTCODE = 'your_shortcode'
 PASSKEY = 'your_passkey'
 INITIATE_URL = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
-# Use the sandbox URL for testing. Change it to the production URL when deploying to production.
-
-
-url = reverse('initiate_payment')
-data = {
-    'phone_number': '+254719575272',
-    'amount': '5'
-}
-
-response = requests.post(url, data=data)
-print(response.json())
-
 
 def signup(request):
     if request.method == 'POST':
-        username = request.POST.get['username']
-        email = request.POST.get['email']
-        password = request.POST.get['password']
-        password2 = request.POST.get['password2']
-        
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
         
         if len(password) < 8:
-            messages.error(request, 'Password must be above eight charachters')
+            messages.error(request, 'Password must be at least eight characters')
             return redirect('signup')
             
         if User.objects.filter(username=username).exists():
-            messages.error(request,'Username already taken.')
+            messages.error(request, 'Username already taken.')
             return redirect('signup')
+            
         if User.objects.filter(email=email).exists():
             messages.error(request, 'Email already exists')
-            return redirect('signup')       
+            return redirect('signup')
         
         if password != password2:
-            messages.error(request, 'Passwords do not Match.') 
+            messages.error(request, 'Passwords do not match.') 
             return redirect('signup')
         
         if not username.isalnum():
-            messages.error(request, "Username must be alphanumeric")
-            return redirect('signup')   
+            messages.error(request, 'Username must be alphanumeric')
+            return redirect('signup')
             
-            myuser = User.objects.create_user(username=username, email=email, password=password)
-            messages.success(request, "Account Created Successfully")
-            return redirect('login')
-        
+        myuser = User.objects.create_user(username=username, email=email, password=password)
+        messages.success(request, 'Account created successfully')
+        return redirect('login')
     else:
         return render(request, 'signup.html')
 
-def login(request):
+def login_user(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        authenticate(username=username,password=password)
- 
-        user = authenticate(request, username=username, password=password)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
 
         if user is not None:
             login(request, user)
@@ -111,88 +89,40 @@ def credit_card(request):
     return render(request, 'credit_card.html')
 
 def charge(request):   
-    amount = 5 
     if request.method == 'POST':
+        amount = 5 
         print('Data', request.POST)
         
         stripe.Customer.create(
-            email = request.POST.get('email'),
-            name = request.POST.get('name'),
+            email=request.POST.get('email'),
+            name=request.POST.get('name'),
         )
         
         return redirect(reverse('success', args=[amount]))
-    
-def successMsg(request, args):
+
+def success_msg(request, args):
     amount = args
-    return render(request, 'base.success.html', {'amount':amount})
+    return render(request, 'base.success.html', {'amount': amount})
 
 def ticket(request):
     print(request.user)
     return render(request, 'ticket.html')
 
-def my_view(request):
-    ipn_url = request.build_absolute_uri(reverse('paypal-ipn'))
-
-    context = {
-        'ipn_url': ipn_url,
-    }
-
-    return render(request, 'my_template.html', context)
-
-'''def payment_process(request):
-    paypal_dict = {
-        'business': settings.PAYPAL_RECEIVER_EMAIL,
-        'amount': '10.00',
-        'currency_code': 'USD',
-        'item_name': 'Virtual Ticket',
-        'invoice': 'unique-invoice-id',
-        'notify_url': 'https://yourdomain.com/paypal/ipn/',  
-        'return_url': 'https://yourdomain.com/payment/success/',
-        'cancel_return': 'https://yourdomain.com/payment/cancel/',
-    }
-    form = PayPalPaymentsForm(initial=paypal_dict)
-    return render(request, 'payment_process.html', {'form': form})
-
-@csrf_exempt
-def payment_success(request):
-    return HttpResponse("Payment Successful")
-
-@csrf_exempt
-def payment_cancel(request):
-    return HttpResponse("Payment Canceled")'''
-
 @csrf_exempt
 def initiate_payment(request):
     if request.method == 'POST':
-        phone_number = request.POST.get('phone_number')
-        amount = request.POST.get('amount')
-
-        payload = {
-            "BusinessShortCode": SHORTCODE,
-            "Password": PASSKEY,
-            "Timestamp": "yyyyMMddHHmmss",
-            "TransactionType": "CustomerPayBillOnline",
-            "Amount": amount,
-            "PartyA": phone_number,
-            "PartyB": SHORTCODE,
-            "PhoneNumber": +254719575272,
-            "CallBackURL": "callback/",
-            "AccountReference": "Test",
-            "TransactionDesc": "Test Payment"
-        }
-
+        url = "https://sandbox.safaricom.co.ke/oauth/v1/generate"
+        querystring = {"grant_type": "client_credentials"}
+        payload = ""
         headers = {
-            'Authorization': 'Bearer {}'.format(get_access_token()),
-            'Content-Type': 'application/json'
+            "Authorization": "Basic SWZPREdqdkdYM0FjWkFTcTdSa1RWZ2FTSklNY001RGQ6WUp4ZVcxMTZaV0dGNFIzaA=="
         }
-
-        response = requests.post(INITIATE_URL, json=payload, headers=headers)
-        data = response.json()
-        
-        # Process response and return appropriate JSON response
-        return JsonResponse(data)
-
-    return JsonResponse({"error": "Only POST requests are allowed"})
+        response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+        print(response.text)
+        # Process the response and return appropriate JSON response
+        return JsonResponse({"success": True})
+    else:
+        return JsonResponse({"error": "Only POST requests are allowed"})
 
 def get_access_token():
     url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
@@ -213,3 +143,42 @@ def callback(request):
         return JsonResponse({"success": True})
 
     return JsonResponse({"error": "Only POST requests are allowed"})
+
+
+'''
+def payment_process(request):
+    paypal_dict = {
+        'business': settings.PAYPAL_RECEIVER_EMAIL,
+        'amount': '10.00',
+        'currency_code': 'USD',
+        'item_name': 'Virtual Ticket',
+        'invoice': 'unique-invoice-id',
+        'notify_url': 'https://yourdomain.com/paypal/ipn/',  
+        'return_url': 'https://yourdomain.com/payment/success/',
+        'cancel_return': 'https://yourdomain.com/payment/cancel/',
+    }
+    form = PayPalPaymentsForm(initial=paypal_dict)
+    return render(request, 'payment_process.html', {'form': form})
+
+@csrf_exempt
+def payment_success(request):
+    return HttpResponse("Payment Successful")
+
+@csrf_exempt
+def payment_cancel(request):
+    return HttpResponse("Payment Canceled")
+'''
+
+
+
+def generate_ticket(request):
+    # Generate a unique ticket number and code
+    ticket_number = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+    ticket_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
+
+    # Get form data
+    full_name = request.POST.get('fullName')
+    email = request.POST.get('email')
+
+    # Render the ticket template with the generated data
+    return render(request, 'ticket_template.html', {'full_name': full_name, 'email': email, 'ticket_number': ticket_number, 'ticket_code': ticket_code})
