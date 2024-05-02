b33e75c147938eb30d4184d7f43dcf298644f8a0
EricNdivo <j.ericndivo@gmail.com>
2024-05-02 10:50:20 +0300

    postgres(datetime)

--- eventmanagement/models.py
+++ eventmanagement/models.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from django.db import models
+
 class Ticket(models.Model):
     full_name = models.CharField(max_length=100)
     email = models.EmailField()
     ticket_number = models.CharField(max_length=8)
     ticket_code = models.CharField(max_length=12)
     number_of_tickets = models.IntegerField(default=1)
+    date = models.DateTimeField(default=datetime.now())
