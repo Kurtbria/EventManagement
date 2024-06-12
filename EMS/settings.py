4a2bd91ee269235f1ac97430d90633357f91baa7
EricNdivo <j.ericndivo@gmail.com>
2024-06-12 11:00:08 +0300

    email(smtp)

--- EMS/settings.py
+++ EMS/settings.py
@@ -154,3 +154,11 @@ CONSUMER_SECRET = '#'
 SHORTCODE = 'your_shortcode'
 PASSKEY = 'your_passkey'
 INITIATE_URL = '#'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'j.ericndivo@gmail.com'
+EMAIL_HOST_PASSWORD = 'wd_22*jmv'
